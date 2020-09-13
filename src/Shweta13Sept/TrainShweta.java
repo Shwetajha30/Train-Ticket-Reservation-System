f19b11bae63a0e84db6f3beea8d22073eafbb5b6
Shwetajha30 <uic.19mca8195@gmail.com>
2020-09-13 13:06:40 +0530

    Add new Shweta 13 Sept 2020

---
+++ src/Shweta13Sept/TrainShweta.java
@@ -0,0 +1,10 @@
+package Shweta13Sept;
+
+public class TrainShweta {
+
+	public static void main(String[] args) {
+		System.out.println("Shweta 13 Sept 2020");
+
+	}
+
+}
---
+++ src/TrainShweta.java
@@ -0,0 +1,9 @@
+
+public class TrainShweta {
+
+	public static void main(String[] args) {
+	System.out.println("13 Sept 2020 Shweta");
+
+	}
+
+}
